84ab19ce2af6666bcd674b43ae0cd9fa2c36a278
Konstantin Vlasov <mrwlasow@gmail.com>
2018-04-03 11:38:56 +0300

    Added partner seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+partners = [
+		{name: "Delimobil", adapter_name: "delimobil"},
+		{name: "Car5", adapter_name: "car5"},
+		{name: "TimCar", adapter_name: "timcar"}
+	]
+
+if Partner.first && ENV['RECREATE_PARTNERS'].blank?
+  puts "You seem to have some industries already. Run with RECREATE_PARTNERS=true rake db:seed"
+else
+  Partner.destroy_all
+  partners.each do |partner|
+    Partner.create partner
+  end
+
+  puts "=== CREATED PARTNERS"
+end
